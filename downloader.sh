2c8261438fd90afbac533e5e80731030ed517784
4urora3night <auroraph4ntom@gmail.com>
2025-06-20 19:52:16 +0100

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-mkdir -p aurora-pac
-mkdir -p aurora-pac/lib
-cd aurora-pac
+mkdir -p earth
+mkdir -p earth/lib
+cd earth
 
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/aurorapac.sh
-chmod +x aurorapac.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/aurorapac.sh
+chmod +x earth.sh
 
 cd lib
 
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/lib/app-installer.sh
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/lib/aur-helper.sh
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/lib/settings.sh
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/lib/terminal-ui.sh
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/lib/tty-ui.sh
-curl -O https://raw.githubusercontent.com/4urora3night/aurora-pac/refs/heads/main/lib/utils.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/app-installer.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/aur-helper.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/settings.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/terminal-ui.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/tty-ui.sh
+curl -O https://raw.githubusercontent.com/4urora3night/earth/refs/heads/main/lib/utils.sh
